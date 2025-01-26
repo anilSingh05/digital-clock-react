cb0ca4f63013172f28054de895202e32c1a26732
Anil Singh <anil05.me@gmail.com>
2025-01-26 12:51:51 -0600

    Digital Clock React

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const formatTime = time.toLocaleTimeString('en-US', 
+    { hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+     }
+  )
+
+  return (
+    <div className="digital-clock">
+      <div className="clock">
+      {formatTime}
+      </div>
+    </div>
+  )
+}
+
+export default App
