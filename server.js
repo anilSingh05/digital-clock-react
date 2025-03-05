a80cafc675c6638fc8760554a22f6bdd0b67e35f
Anil Singh <anil05.me@gmail.com>
2025-03-05 00:06:32 -0600

    Added serrver.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// filepath: /Users/singhanil/Desktop/myProjects/2025/VS-Code/learning-react/digital-clock-react/server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
